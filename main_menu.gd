5f3bbb733c40987cfdfad91a2e8d03b439bf21c7
weghuz <rasmus.grey@hotmail.se>
2024-02-10 22:06:48 +0100

    Initial commit

---
+++ main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+
+func _on_start_game_button_pressed():
+	get_tree().change_scene_to_file('res://level1.tscn')
+	pass
+
+func _on_settings_button_pressed():
+	pass # Replace with function body.
+
+func _on_exit_button_pressed():
+	get_tree().quit()
