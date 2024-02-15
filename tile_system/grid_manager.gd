d038156d870932ec13e93c1acfc06e9982d44a48
weghuz <rasmus.grey@hotmail.se>
2024-02-15 21:49:49 +0100

    Clean up project with new folder for assets.
    Clean up tilemap code and make Tile handle more of the logic itself.

--- tile_system/grid_manager.gd
+++ tile_system/grid_manager.gd
@@ -3,69 +3,44 @@ class_name TileManager
 extends TileMap
 
 @export var grid_size: Vector2 = Vector2(8, 8)
-
-enum {WHITE, BLACK, BALL, BALL2}
 @export var tiles: Array[Vector2]
-var current_tilemap: Array[Tile] = []
 
-var last_tile: Tile
-var last_tile_set: bool = false
+var tilemap: Array[Tile] = []
+var focus = false;
 
-# Called when the node enters the scene tree for the first time.
+# // INITIALIZATION
 func _ready():
 	seed_tiles()
-	draw_tiles()
-	pass # Replace with function body.
 
 func seed_tiles():
 	for x in grid_size.x:
 		for y in grid_size.y:
-			if int(x) % 2 == int(y) % 2:
-				current_tilemap.append(Tile.new(Vector2(x, y), tiles[WHITE]))
-				continue
-			current_tilemap.append(Tile.new(Vector2(x, y), tiles[BLACK]))
+			var new_tile = Tile.new(Vector2(x, y), tiles, Tile.TILE.BLACK if int(x + y) % 2 == 0 else Tile.TILE.WHITE);
+			tilemap.append(new_tile)
+			draw_tile(new_tile)
 
-func draw_tile(pos: Vector2, tile: Vector2):
-	set_cell(0, pos, 0, tile)
+			new_tile.tile_changed.connect(draw_tile)
+			new_tile.tile_focused.connect(focus_tile)
 
-func draw_tiles():
-	for tile in current_tilemap:
-		draw_tile(tile.position, tile.tile)
+# // STATE
+func draw_tile(tile: Tile):
+	set_cell(0, tile.position, 0, tile.tile)
 
+func focus_tile(_tile: Tile):
+	focus = true
+
+# // GAME LOOP
 func _input(event):
-	if event is InputEventMouseButton && event.is_pressed():
-		var mouse_pos = get_global_mouse_position()
-		var tile_pos = local_to_map(mouse_pos)
-		if tile_pos.x < grid_size.x && tile_pos.y < grid_size.y:
-			set_tile(Tile.new(tile_pos, tiles[BALL2]))
-			draw_tiles()
-	elif event is InputEventMouseMotion:
-		var mouse_pos = get_global_mouse_position()
-		var tile_pos = local_to_map(mouse_pos)
-		if tile_pos.x < grid_size.x && tile_pos.y < grid_size.y:
-			if get_tile(tile_pos).tile == tiles[BALL2]:
-				return
-			reset_last_tile()
-			set_last_tile(tile_pos)
-			set_tile(Tile.new(tile_pos, tiles[BALL]))
-			draw_tiles()
-		else:
-			reset_last_tile()
-			draw_tiles()
-			
-func reset_last_tile():
-	if(last_tile_set && get_tile(last_tile.position).tile != tiles[BALL2]):
-		set_tile(last_tile)
+	if focus && event.is_pressed():
+		focus = false
+	elif focus:
+		return
 
-func set_last_tile(new_position: Vector2):
-	last_tile = Tile.new(new_position, get_tile(new_position).tile)
-	last_tile_set = true
+	var mouse_pos = get_global_mouse_position()
+	var tile_pos = local_to_map(mouse_pos)
+	for tile in tilemap:
+		tile.handle_input(event, tile_pos)
 
-func get_tile(tile_position: Vector2):
-	var index = (tile_position.x * grid_size.x) + tile_position.y;
-	if current_tilemap.size() >= index:
-		return current_tilemap[index]
-	return null
-
-func set_tile(new_tile: Tile):
-	current_tilemap[(new_tile.position.x * grid_size.x) + new_tile.position.y] = new_tile
+# // UTILS
+func get_tile(pos: Vector2) -> Tile:
+	return tilemap[int(pos.x) * int(grid_size.x) + int(pos.y)]
--- tile_system/tile.gd
+++ tile_system/tile.gd
@@ -1,9 +1,29 @@
 extends Resource
 class_name Tile
 
+enum TILE {WHITE, BLACK, BALL, BALL2}
 @export var position: Vector2
 @export var tile: Vector2
+@export var tiles: Array[Vector2]
+var initial_tile
 
-func _init(initial_position: Vector2, initial_tile: Vector2):
+signal tile_changed(tile: Tile)
+signal tile_focused(tile: Tile)
+
+func _init(initial_position: Vector2, initial_tiles: Array[Vector2], new_tile: TILE):
+	tiles = initial_tiles;
 	position = initial_position
-	tile = initial_tile
+	tile = tiles[new_tile]
+	initial_tile = tile;
+
+func handle_input(event: InputEvent, mouse_tile_pos: Vector2):
+	if mouse_tile_pos == position:
+		if event.is_pressed():
+			tile = tiles[TILE.BALL2]
+			tile_focused.emit(self)
+		else:
+			tile = tiles[TILE.BALL]		
+		tile_changed.emit(self)
+	else:
+		tile = initial_tile
+		tile_changed.emit(self)
